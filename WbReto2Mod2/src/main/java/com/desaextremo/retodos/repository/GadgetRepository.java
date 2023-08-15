22462e91d753495c6c58fd928faaf1610d1d5dee
Camilo Cubillos <131101280+CamiloCubillos89@users.noreply.github.com>
2023-08-14 23:47:58 -0500

    Delete WbReto2Mod2 directory

--- WbReto2Mod2/src/main/java/com/desaextremo/retodos/repository/GadgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.desaextremo.retodos.repository;
-
-import com.desaextremo.retodos.entity.Gadget;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GadgetRepository extends MongoRepository<Gadget,Integer> {
-}
